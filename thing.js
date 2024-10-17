9ddcb859e86b694d35a678c594509c303b869ede
Sjeakx <issuers_grad_0v@icloud.com>
2024-10-17 03:55:29 +0000

    trhig

---
+++ thing.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Display External Website</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      height: 100vh;
+    }
+    iframe {
+      width: 100%;
+      height: 100%;
+      border: none;
+    }
+  </style>
+</head>
+<body>
+  <iframe id="externalSite" src="https://mac-web.github.io/macvg/" loading="lazy"></iframe>
+</body>
+</html>
